49dfc70a7976566cb58cc85602d73dcd928760de
Laura <erdaradun.gaztea@gmail.com>
2021-02-10 13:21:29 +0100

    implemented new topscorer format

--- interpreters/topscorer_formats.py
+++ interpreters/topscorer_formats.py
@@ -16,6 +16,16 @@ def match_topscorer_format_2(line):
         line)
 
 
+def match_topscorer_format_3(line):
+    """
+    Example line:
+    Heidar Sigurjonsson (Throttur)     9  [later changed name to Heidar Helguson]
+    """
+    return re.match(
+        r'\s*[\w\s]+\([\w\s]+\)\s*[0-9]+.*',
+        line)
+
+
 def read_topscorer_format_1(line, current_goals, default_country):
     # update goal score, if present
     goalscore_re = re.match(r'\s*([0-9]+)\s*-\s*', line)
@@ -55,9 +65,37 @@ def read_topscorer_format_2(line, current_goals, default_country):
     return Topscorer(player_line.strip(), int(current_goals), team, default_country), current_goals
 
 
+def read_topscorer_format_3(line, current_goals, default_country):
+    if not match_topscorer_format_3(line):
+        return None, current_goals
+
+    # extract player name
+    player_re = re.match(r'\s*([^(]+)\(', line)
+    if not player_re:
+        return None, current_goals
+    player = player_re.group(1).strip()
+
+    # read player team
+    team_re = re.search(r'\s+\(([^)]+)\)', line)
+    if not team_re:
+        return None, current_goals
+    team = TeamDictionary.get(team_re.group(1).strip())
+
+    # update goal score, if present
+    goal_line = line[team_re.end():]
+    goalscore_re = re.search(r'\s*([0-9]+).*', line)
+    if goalscore_re:
+        current_goals = goalscore_re.group(1).strip()
+
+    return Topscorer(player, int(current_goals), team, default_country), current_goals
+
+
 def detect_topscorer_format(line):
     if line is not None:
-        if match_topscorer_format_2(line):
+        if match_topscorer_format_3(line):
+            print("Detected scorer format no. 3.")
+            return read_topscorer_format_3
+        elif match_topscorer_format_2(line):
             print("Detected scorer format no. 2.")
             return read_topscorer_format_2
         elif match_topscorer_format_1(line):
